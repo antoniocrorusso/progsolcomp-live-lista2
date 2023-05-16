6b3305b70330bfd4f85b0961029fe11d40cb3601
Antonio Russo <antonio.cror@gmail.com>
2023-05-16 19:13:52 -0300

    Pasta dos exercicios criadas, problema 1 e pdf dos problemas

---
+++ Exercicios/Problema1.java
@@ -0,0 +1,36 @@
+/* Nome do Aluno: Antonio Carlos Rodrigo de Oliveira Russo
+* RA: 12523118842
+* Nome do Programa: Problema 1
+* Data: 21/03/2023
+*/
+
+import java.util.Scanner;
+
+public class Problema1{
+    public static void main(String[] args) {
+        
+        Double num1, num2;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite dois números para calcular o maior.");
+
+        System.out.println("Numero 1: ");
+        num1 = input.nextDouble();
+        System.out.println("Numero 2: ");
+        num2 = input.nextDouble();
+
+        if(num1 > num2){
+            System.out.println("O numero 1 (" + num1 + ") é o maior.");
+        } 
+        else if(num2 > num1){
+            System.out.println("O numero 2 (" + num2 + ") é o maior.");
+        }
+        else{
+            System.out.println("Os números possuem o mesmo valor.");
+        }
+
+        input.close();
+    }
+
+}
